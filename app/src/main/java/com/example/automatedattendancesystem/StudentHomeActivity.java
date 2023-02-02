0a779e246f2b605235cbb223fba0fbc981592864
Mohitdk <mohitdk2@gmail.com>
2023-02-02 14:58:28 +0530

    Student and Teacher home screen UI introduced

--- app/src/main/java/com/example/automatedattendancesystem/StudentHomeActivity.java
+++ app/src/main/java/com/example/automatedattendancesystem/StudentHomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.automatedattendancesystem;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,10 @@ public class StudentHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_home);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.show();
+
     }
 }
--- app/src/main/java/com/example/automatedattendancesystem/TeacherHomeActivity.java
+++ app/src/main/java/com/example/automatedattendancesystem/TeacherHomeActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class StudentHome extends AppCompatActivity {
+public class TeacherHomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_home);
+        setContentView(R.layout.activity_teacher_home);
     }
 }
